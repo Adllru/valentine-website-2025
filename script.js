536d94ce0ff2562eade67bce9dbc35e502576f0e
Adil <adiilbeksultanov@gmail.com>
2026-06-25 03:08:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, жанусям...`;
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}, девушка с красивыми глазами...`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
